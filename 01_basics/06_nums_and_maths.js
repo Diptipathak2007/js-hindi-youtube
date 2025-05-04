dc33cbbc3af0b23663020518ca5564f404b142f5
Diptipathak2007 <diptipathak019@gmail.com>
2025-05-04 11:50:42 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score=400;
+const balance =new Number(100);
+//console.log(balance);
+//console.log(score);
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+const othernumber=123.8986;
+//console.log(othernumber.toPrecision(3));
+const hundreds =10000000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++++++++++maths+++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 5, 6, 7));
+// console.log(Math.max(4, 5, 6, 7));
+
+console.log(Math.random());
+console.log((Math.random() * 10) +1); 
+console.log((Math.floor(Math.random()*10)+1));
+
+const min=10;
+const max=20;
+console.log((Math.floor(Math.random()*(max-min+1)+min)));
