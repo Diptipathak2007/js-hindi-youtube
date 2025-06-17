1afe52006694b9fb8d31be825c472ab914143a87
Diptipathak2007 <diptipathak019@gmail.com>
2025-06-17 19:09:51 +0530

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+//while loop
+//do while loop
+// let index=0;
+// while(index<=10){
+//     console.log(`value of index:${index}`);
+//     index++;
+// }
+// let myarray=["apple","banana","orange"];
+// let arr=0;
+// while(arr<myarray.length){
+//     console.log(`value of arr:${myarray[arr]}`);
+//     arr++;
+// }
+
+let score=1;
+do {
+   console.log(`score is ${score}`);
+   score=score+1;
+} while (score<10);
